c23e3d05f1a3fd64c1ad90e1e1f8b76bde546966
pedro_tentugal <pedrotentugal@gmail.com>
2026-03-16 23:56:09 +0000

    Restoring deleted projects by mistake and adding new ones too. Set resolver 3 for parent to address the reference to modern rust.

---
+++ ch2/add-with-lifetimes/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = 20;
+    let b = 30;
+
+    let sum = add_with_lifetimes(a, b);
+    println!("{}", sum)
+}
+
+fn add_with_lifetimes<'a,'b>(i: &'a i32, j: &'b i32) -> i32 {
+    ªi + ªj // adds values, not the references to them
+}
