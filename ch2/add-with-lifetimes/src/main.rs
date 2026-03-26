7e9b51e196791059a23bfe91ad09ed093c0e419e
pedro_tentugal <pedrotentugal@gmail.com>
2026-03-26 14:09:08 +0000

    lifetimes add fixes

--- ch2/add-with-lifetimes/src/main.rs
+++ ch2/add-with-lifetimes/src/main.rs
@@ -2,10 +2,10 @@ fn main() {
     let a = 20;
     let b = 30;
 
-    let sum = add_with_lifetimes(a, b);
+    let sum = add_with_lifetimes(&a, &b);
     println!("{}", sum)
 }
 
 fn add_with_lifetimes<'a,'b>(i: &'a i32, j: &'b i32) -> i32 {
-    ªi + ªj // adds values, not the references to them
+    *i + *j // adds values, not the references to them
 }
